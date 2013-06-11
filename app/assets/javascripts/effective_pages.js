eaa8327474cf64024f78cd3135cb4ad4195988f5
Matt Riemer <matthew@agilestyle.com>
2013-06-11 14:59:51 -0600

    ok

--- app/assets/javascripts/effective_pages.js
+++ app/assets/javascripts/effective_pages.js
@@ -1,10 +1,1 @@
-//= require ../mercury/distro/mercury
-//= require ../mercury/distro/dependencies/marked-0.2.8
-//= require ../mercury/distro/dependencies/rangy-core
-//= require ../mercury/distro/regions/html
-//= require ../mercury/distro/regions/markdown
-//= require ../mercury/distro/regions/image
-//= require ../mercury/distro/regions/plain
-//= require ../mercury/distro/regions/text
-
 //= require_tree ./effective_pages
