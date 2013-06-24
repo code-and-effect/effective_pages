7afc5832cb8f920110605bff323afd6a0780ce49
Matt Riemer <matthew@agilestyle.com>
2013-06-24 12:03:06 -0600

    Better initializer

--- lib/generators/templates/effective_pages.rb
+++ lib/generators/templates/effective_pages.rb
@@ -3,5 +3,8 @@
 EffectivePages.setup do |config|
   config.pages_table_name = :pages
   config.templates_path = 'templates/'  # This is relative to your app/views/ directory
-  config.authorization_method = Proc.new { |controller, action, resource| can?(action, resource) }
+
+  # Use CanCan: can?(action, resource)
+  # Use effective_roles:  resource.roles_match_with?(current_user)
+  config.authorization_method = Proc.new { |controller, action, resource| true }
 end
