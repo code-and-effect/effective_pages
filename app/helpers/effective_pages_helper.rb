8034b3308041a2ebaf849df5578d91ac6dbeaa53
Matt Riemer <matthew@agilestyle.com>
2015-02-03 14:03:18 -0700

    reorganize helpers

--- app/helpers/effective_pages_helper.rb
+++ app/helpers/effective_pages_helper.rb
@@ -1,10 +1,30 @@
 module EffectivePagesHelper
-  def effective_page_header_tags(options = {})
-    @page ||= nil
 
-    [ content_tag(:title, (@page_title || options[:title] || @page.try(:title))),
-      "<meta content='#{options[:meta_description] || @page.try(:meta_description)}' name='description' />",
-    ].join("\n").html_safe
+  def effective_pages_body_classes
+    [
+      params[:controller].parameterize,
+      params[:action],
+      ((user_signed_in? ? 'signed-in' : 'not-signed-in') rescue nil),
+      (@page.template rescue nil),
+      @body_classes
+    ].compact.join(' ')
+  end
+
+  def effective_pages_header_tags
+    [
+      content_tag(:title, effective_pages_site_title),
+      effective_pages_meta_description_tag
+    ].compact.join("\n").html_safe
+  end
+
+  def effective_pages_site_title
+    (@page_title ? @page_title.to_s : "#{params[:controller].try(:titleize)} #{params[:action].try(:titleize)}") + EffectivePages.site_title_suffix.to_s
+  end
+
+  def effective_pages_meta_description_tag
+    if @page.try(:meta_description).present?
+      "<meta content='#{@page.try(:meta_description)}' name='description' />"
+    end
   end
 
   def application_root_to_effective_pages_slug
