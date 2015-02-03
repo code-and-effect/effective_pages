a4a3fbbe0f0767fdb571485ebe0b206f140132d8
Matt Riemer <matthew@agilestyle.com>
2015-02-03 14:03:27 -0700

    better comments in the initializer

--- lib/generators/templates/effective_pages.rb
+++ lib/generators/templates/effective_pages.rb
@@ -5,13 +5,18 @@ EffectivePages.setup do |config|
   config.menus_table_name = :menus
   config.menu_items_table_name = :menu_items
 
+  # The directory where your page templates live
+  # Any files in this directory will be automatically available when
+  # creating/editting an Effective::Page from the Admin screens
   # Relative to app/views/
   config.pages_path = '/effective/pages/'
 
   # Excluded Pages
+  # Any page templates from the above directory that should be excluded
   config.excluded_pages = []
 
   # Excluded Layouts
+  # Any app/views/layouts/ layout files that should be excluded
   config.excluded_layouts = [:admin]
 
   # Use CanCan: can?(action, resource)
@@ -21,10 +26,13 @@ EffectivePages.setup do |config|
   # Layout Settings
   # Configure the Layout per controller, or all at once
 
-  # config.layout = 'application'   # The layout for the EffectivePages admin screen
+  # The layout for the EffectivePages admin screen
   config.layout = {
     :admin => 'application'
   }
+
+  # This string will be appended to the effective_pages_header_tags title tag
+  config.site_title_suffix = " | #{Rails.application.class.name.split('::').first.titleize}"
 
   # SimpleForm Options
   # This Hash of options will be passed into any simple_form_for() calls
@@ -42,8 +50,8 @@ EffectivePages.setup do |config|
 
   # All effective_page menu options
   config.menu = {
-    :apply_active_class => true,
-    :maxdepth => 2
+    :apply_active_class => true,  # Add an .active class to the appropriate li item based on current page url
+    :maxdepth => 2                # 2 by default, strict bootstrap3 doesnt support dropdowns in your dropdowns
   }
 
 end
