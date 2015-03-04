4bbaf9516a50d812381efe9eed082161e69fe154
Matt Riemer <matthew@agilestyle.com>
2015-03-04 10:30:51 -0700

    Add menu item visibility report on Admin::Menu#edit

--- app/helpers/effective_menus_admin_helper.rb
+++ app/helpers/effective_menus_admin_helper.rb
@@ -1,8 +1,6 @@
 module EffectiveMenusAdminHelper
 
-  def visible_badge(menu_item, stack, comparator, is_role = false)
-    binding.pry if is_role
-
+  def visible_badge(menu_item, stack, comparator)
     visible = comparator.call(menu_item) && stack.all? { |parent_menu_item| parent_menu_item.lft == 1 || comparator.call(parent_menu_item) }
     content_tag(:span, (visible ? 'YES'.freeze : 'NO'.freeze), :class => "label label-#{(visible ? 'primary' : 'warning')}")
   end
