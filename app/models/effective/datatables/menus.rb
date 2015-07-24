e425396bcfa927d281a3e7b426862db4ea56fafa
Matt Riemer <matthew@agilestyle.com>
2015-07-24 15:36:55 -0600

    upgrade effective_datatables

--- app/models/effective/datatables/menus.rb
+++ app/models/effective/datatables/menus.rb
@@ -2,10 +2,12 @@ if defined?(EffectiveDatatables)
   module Effective
     module Datatables
       class Menus < Effective::Datatable
-        table_column :id
+        datatable do
+          table_column :id
 
-        table_column :title
-        table_column :actions, :sortable => false, :filter => false, :partial => '/admin/menus/actions'
+          table_column :title
+          table_column :actions, :sortable => false, :filter => false, :partial => '/admin/menus/actions'
+        end
 
         def collection
           Effective::Menu.all
--- app/models/effective/datatables/pages.rb
+++ app/models/effective/datatables/pages.rb
@@ -2,16 +2,18 @@ if defined?(EffectiveDatatables)
   module Effective
     module Datatables
       class Pages < Effective::Datatable
-        default_order :title, :asc
-        default_entries :all
+        datatable do
+          default_order :title, :asc
+          default_entries :all
 
-        table_column :id, :visible => false
+          table_column :id, :visible => false
 
-        table_column :title
-        table_column :slug
-        table_column :draft
+          table_column :title
+          table_column :slug
+          table_column :draft
 
-        table_column :actions, :sortable => false, :filter => false, :partial => '/admin/pages/actions'
+          table_column :actions, :sortable => false, :filter => false, :partial => '/admin/pages/actions'
+        end
 
         def collection
           Effective::Page.all
