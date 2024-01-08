625c33c7f8211726290fb112df03834891f1d7e1
Matt Riemer <matthew@codeandeffect.com>
2024-01-08 11:11:19 -0700

    Caching carousels

--- app/helpers/effective_carousels_helper.rb
+++ app/helpers/effective_carousels_helper.rb
@@ -1,23 +1,14 @@
 # frozen_string_literal: true
 module EffectiveCarouselsHelper
 
-  def render_carousel(name, options = {}, &block)
-    name = name.to_s
-    carousel = Array(EffectivePages.carousels).find { |carousel| carousel.to_s == name }
+  def render_carousel(name, carousel_options = {})
+    carousel = Array(EffectivePages.carousels).find { |carousel| carousel.to_s == name.to_s }
 
     if carousel.blank?
       raise("unable to find carousel #{name}. Please add it to config/initializers/effective_pages.rb")
     end
 
-    carousel_items = Effective::CarouselItem.sorted.deep.where(carousel: carousel)
-    return if carousel_items.blank?
-
-    if block_given?
-      yield(carousel_items); nil
-    else
-      render('effective/carousels/carousel', carousel: carousel, carousel_items: carousel_items, carousel_options: options)
-    end
-
+    render('effective/carousels/carousel', carousel: carousel, carousel_options: carousel_options)
   end
 
 end
