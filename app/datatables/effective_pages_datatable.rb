ab48ff84d25d40667e162c279a2cce632016931c
Matt Riemer <matthew@codeandeffect.com>
2025-04-24 14:44:33 -0600

    Remove unneeded search: :string fields

--- app/datatables/effective_pages_datatable.rb
+++ app/datatables/effective_pages_datatable.rb
@@ -37,7 +37,7 @@ class EffectivePagesDatatable < Effective::Datatable
     col :menu_parent, search: { collection: admin_menu_parent_collection(), grouped: true }
     col :menu_position, visible: false
 
-    col :page_banner, search: :string, visible: false
+    col :page_banner, visible: false
 
     col :authenticate_user, visible: false
     col :roles, visible: false
